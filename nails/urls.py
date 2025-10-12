088b465b98c751f2697a96aa9f050cb6bcf4e8da
NikoQP <119464249+NikoQuicasan@users.noreply.github.com>
2025-10-11 20:56:02 -0500

    Avances en contactanos

--- nails/urls.py
+++ nails/urls.py
@@ -21,5 +21,6 @@ from nails_app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'), 
-]
+    path('contacto/', views.contacto, name='contacto')
+    ]
 from django.conf import settings
--- nails_app/views.py
+++ nails_app/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .models import MensajeContacto
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html')
+
+def contacto(request):
+    if request.method == 'POST':
+        nombre = request.POST.get('nombre')
+        correo = request.POST.get('correo')
+        asunto = request.POST.get('asunto')
+        mensaje = request.POST.get('mensaje')
+
+        MensajeContacto.objects.create(
+            nombre=nombre,
+            correo=correo,
+            asunto=asunto,
+            mensaje=mensaje
+        )
+
+        return redirect('contacto')  
+
+    return render(request, 'contactanos.html')
